9f4e79b89191985b302dd5a3f1f83afe2f45f2f2
Joanna-Golofit <sunderlajsa@gmail.com>
2022-01-25 15:20:21 +0100

    actions komentarz

--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,6 +1,6 @@
 import { BUY_CAKE } from "./cakeTypes";
 
-//action creator is a function that returns an object : 
+//action creator is a function that creates/returns an action, which is an object with type (?and payload) property: 
 export const buyCake = () => {
   return { type: BUY_CAKE };
 };
