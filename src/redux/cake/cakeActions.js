7ae1b77eeb5541dfc76ba9987145ae4877201d1f
Joanna-Golofit <sunderlajsa@gmail.com>
2022-01-25 15:36:33 +0100

    stworzony reducer, update pliku readme

--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -2,5 +2,8 @@ import { BUY_CAKE } from "./cakeTypes";
 
 //action creator is a function that creates/returns an action, which is an object with type (?and payload) property: 
 export const buyCake = () => {
-  return { type: BUY_CAKE };
+  return {
+    type: BUY_CAKE,
+    info: "First redux action created"
+  };
 };
--- src/redux/cake/cakeTypes.js
+++ src/redux/cake/cakeTypes.js
@@ -1,2 +1,2 @@
-// action is an object with type:
+// action is an object with type property, and here we declare property:
 export const BUY_CAKE = "BUY_CAKE"
